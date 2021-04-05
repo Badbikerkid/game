2e17b7ed99162f66699ab83009322102107cb1f9
Badbikerkid <77379621+Badbikerkid@users.noreply.github.com>
2021-04-05 13:07:09 +0530

    Add files via upload

---
+++ AndEvenMoreQualityCode.java
@@ -0,0 +1,178 @@
+import java.util.Scanner;
+public class AndEvenMoreQualityCode {
+
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+		
+		String userInput = "";
+		
+		System.out.println("Story Of THE Russian");
+		System.out.println("Made By TapoTap#8245");
+		
+		System.out.println("LOCTAION: Russia       DATE: 1955.11.03");
+		System.out.println("");
+		System.out.println("Its cold outside, you are in a bar, drunk as ussual. ");
+		System.out.println("A strange man calls you, he says that there is an emergency mission.");
+		System.out.println("You wonder who is he. Then it hits you: Its the General!");
+		System.out.println("You accept his mission, and you go with him...");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("LOCTAION: Germany      DATE: 1955.11.22");
+		System.out.println("");
+		System.out.println("PILOT: We can not land, they will detect you. You need to jump with the parachute!");
+		System.out.println("Do you jump? (Y/N)");
+		userInput = input.nextLine();
+		
+		if(userInput.equalsIgnoreCase("Y")) {
+	        System.out.println("You jump out of the plane, the parachute get activated, you slowly land in the military base.");
+	        System.out.println("OBJECTIVE: Kill The Officer");
+	        System.out.println("A guard spots you, do you want to kill him? (Y/N)");
+	        userInput = input.nextLine();
+	        
+	    if(userInput.equalsIgnoreCase("Y")) {    
+	    	System.out.println("You managed to take him down silently.");
+	    	System.out.println("You figure out that he is going to meet with the Officer soon.");
+	    	System.out.println("Do you want to dress up as him and then hide the body?(Y/N)");
+	    	userInput = input.nextLine();
+	    	
+	    if(userInput.equalsIgnoreCase("Y")) {	
+	    	System.out.println("The officer finds you and calls you in his office");
+	    	System.out.println("He talks about a SPY in the base.");
+	    	System.out.println("He places 2 cups of tea on the table then turns around");
+	    	System.out.println("Do you poison his tea? (Y/N)");
+	    	userInput = input.nextLine();
+    
+        if(userInput.equalsIgnoreCase("N")) {
+        	System.out.println("He figures out that you are the SPY, then shoots you in the head");
+            System.out.println("YOU DIED");
+            System.exit(0);
+        }
+        
+        if(userInput.equalsIgnoreCase("Y")) {
+        	System.out.println("He drinks the tea, then suffocates and falls to the ground, he died");
+            System.out.println("You exit the office, you go near a fence, do you climb it? (Y/N)");
+            userInput = input.nextLine();
+
+        if(userInput.equalsIgnoreCase("Y")) {
+        	System.out.println("You atempt to climb the fence, you get shocked and die on the spot");
+            System.out.println("YOU DIED");
+            System.exit(0);
+        }
+        if(userInput.equalsIgnoreCase("N")) {
+        	System.out.println("You go to the entrance and the guard lets you out, the PILOT is waiting for you.");
+            System.out.println("You board the helicopter, and you leave and go back to Russia");
+            System.out.println("You are happy to say: Mission Completed");
+            System.out.println("");
+            System.out.println("STEALTHY ENDING");
+            System.exit(0);
+        }
+        }
+	    }
+
+        if(userInput.equalsIgnoreCase("N")) {
+            System.out.println("The officer found you, you take the dead soldiers pistol and shoot him in the head.");
+            System.out.println("Everybody heard that, they started the alarms, they are searching for you.");
+            System.out.println("Do you want to call help with the officers ULTRA range radio? (Y/N)");
+            userInput = input.nextLine();
+            
+         if(userInput.equalsIgnoreCase("N")) {   
+        	 System.out.println("You get TERMINATED by the germans.");
+        	 System.out.println("YOU DIED");
+        	 System.exit(0);
+         }
+        
+        if(userInput.equalsIgnoreCase("Y")) {
+        	System.out.println("Bombs start falling from the sky, you start running to the side of the military base.");
+        	System.out.println("Do you jumped the damaged fence? (Y/N)");
+        	userInput = input.nextLine();
+        
+        if(userInput.equalsIgnoreCase("N")) {
+            System.out.println("A soldier shoots you in the back when heading for the entrance.");
+            System.out.println("YOU DIED");
+            System.exit(0);
+        }
+        if(userInput.equalsIgnoreCase("Y")) {
+            System.out.println("You escape wounded, the PILOT is waiting for you to get in, you board the helicopter");
+            System.out.println("You board the helicopter, and you take one last look at the base, and a HUGE bomb is droped and it explodes.");
+            System.out.println("You go back to russia, and you can say: Mission Completed");
+            System.out.println("");
+            System.out.println("BASE TERMINATED ENDING");
+            System.exit(0);
+        }
+        }
+
+        }
+	    }
+	        
+	    if(userInput.equalsIgnoreCase("N")) {
+	    	System.out.println("The guard shoots you, you are wounded");
+	    	System.out.println("You get thrown into MAXIMUM SECURITY");
+	    	System.out.println("You get tortuted to death there");
+	    	System.out.println("YOU DIED");
+	    	 System.exit(0);
+	    }
+		}
+		
+		if(userInput.equalsIgnoreCase("N")) {
+			System.out.println("");
+			System.out.println("You do not jump, you say, no need, we can land.");
+			System.out.println("You get shot down...");
+			System.out.println("You are alive, but wounded.");
+			System.out.println("You get thrown in jail. NEW MISSION: Get back to Soviet Russia");
+			System.out.println("You realise, the pilot is dead, and then you just go to sleep, and just stay in there...");
+			System.out.println("");
+			System.out.println("LOCATION: German Prison      DATE: 1956.01.23");
+			System.out.println("");
+			System.out.println("You are sick of this prison. You want home, back to that bar... You hatch a plan to distract the guard");
+			System.out.println("The door is left open, but... There is a guard, do you try to take him down? (Y/N)");
+			userInput = input.nextLine();
+            
+            if(userInput.equalsIgnoreCase("Y")) {
+            	System.out.println("You get a baton in your face, you fall back and break your head");
+                System.out.println("YOU DIED");
+                System.exit(0);
+            }
+            if(userInput.equalsIgnoreCase("N")){
+            	System.out.println("You are probably right, it can end you up in a worse place");
+            	System.out.println("You think that maybe you can throw a rock and distract the guard for a bit.");
+            	System.out.println("Will you throw a rock to distract the guard? (Y/N)");
+            	userInput = input.nextLine();
+            	
+            if(userInput.equalsIgnoreCase("N")) {
+            	System.out.println("The doors to your freedom close, and you will never get that chance again.");
+            	System.out.println("You end up rotting in that prison for the rest of your life");
+            	System.out.println("");
+            	System.out.println("ROTTEN ENDING");
+            	 System.exit(0);
+            }
+            if(userInput.equalsIgnoreCase("Y")) {
+            	System.out.println("The guard checks the noise out, you go in the main part of the prison.");
+            	System.out.println("You get spoted by a guard, do you keep runing? (Y/N)");
+            	userInput = input.nextLine();
+            
+            if(userInput.equalsIgnoreCase("Y")) {
+            	System.out.println("You run to the exit door, you find a helicopter, you board it, you go back to russia.");
+            	System.out.println("You did not complete the mission");
+            	System.out.println("");
+            	System.out.println("ESCAPED ENDING");
+            	 System.exit(0);
+            }
+            
+            if(userInput.equalsIgnoreCase("N")) {
+            	System.out.println("You try to fight the guard...");
+            	System.out.println("He peper-sprays you in the face and then, you end up in MAXIMUM SECURITY");
+                System.out.println("You get tortured to death in there.");
+                System.out.println("YOU DIED");
+                System.exit(0);
+            }
+            }
+            }
+
+
+		}
+        
+
+        
+	}
+
+}
